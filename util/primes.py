b7af0523e7d7370a43dba4121870ff02e9e2babe
Ronald Kaiser <raios.catodicos@gmail.com>
2010-10-17 16:11:17 -0200

    Consolidated version of prime number seed generator.

---
+++ util/primes.py
@@ -0,0 +1,24 @@
+import math
+
+#Helper for prime number manipulation
+
+#Sieve algorithm: (Returns a list of prime numbers < n)
+#Credits:
+    # Code from: <dickinsm@gmail.com>, Nov 30 2006
+    # http://groups.google.com/group/comp.lang.python/msg/f1f10ced88c68c2d
+def primes(n):
+	if n <= 2:
+		return []
+	sieve = range(3, n, 2)
+	top = len(sieve)
+	for si in sieve:
+		if si:
+			bottom = (si*si - 3) // 2
+			if bottom >= top:
+				break
+			sieve[bottom::si] = [0] * -((bottom - top) // si)
+	return [2] + [el for el in sieve if el]
+
+max = 1500000
+list = primes(max)
+limit = len(list)
--- util/seed.py
+++ util/seed.py
@@ -1,34 +1,16 @@
 import random
+import primes
 
 #Helper for seed generation...
-#TODO: check intersection given different seeds
+#TODO: check intersection given different seeds (Solved using prime numbers as seeds)
 
-#Method that checks if a given odd number is prime (Naive method)
-def is_prime(n):
-	factors = range(3, n, 2)
-	for f in factors:
-		if n % f == 0:
-			return 0
-	return 1
-
-#Method that returns the n-th prime number
-def get_prime(n):
-	if n == 1: 
-		print "prime number found!: 2"
-		return 2
-	get_prime2(n)
-
-#Done this to avoid making (if n == 1) test over and over..
-def get_prime2(n):
-	#Get only odd numbers first
-	prime = 3 + ((n - 2)*2)
-	if is_prime(prime):
-		print "prime number found!: ", prime
-		return prime
-	get_prime2(n+1)
-
+#A seed is defined giving an index to a prime number list.
+#If the index is higher than the prime list limit, it uses the limit value as an index instead.
 def set_seed(n):
-	random.seed(get_prime(n))
+	if n > primes.limit:
+		n = primes.limit
+	print "Prime found!: ", primes.list[n]
+	random.seed(primes.list[n])
 
 def take_random_number():
 	"Rounds by 2 decimal."
@@ -41,4 +23,4 @@ def generate_random_numbers(seed, n):
 
 if __name__ == "__main__":
 	"Testing..."
-	print generate_random_numbers(6, 10)	
+	print generate_random_numbers(99998, 10)	
